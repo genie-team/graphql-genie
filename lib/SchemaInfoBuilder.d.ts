f56165ddb8bae150577a7f5e4729b3e99258038a
Corey Jepperson <corey@entropy.cc>
2018-05-22 12:55:04 -0500

    remove lib from git

--- lib/SchemaInfoBuilder.d.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLSchema, IntrospectionType } from 'graphql';
-export default class SchemaInfoBuilder {
-    private schema;
-    private schemaInfo;
-    constructor(schema: GraphQLSchema);
-    getSchemaInfo(): Promise<IntrospectionType[]>;
-    private addDirectiveFromAST(astDirective, schemaInfo, path);
-    private buildSchemaInfo(schema);
-}
--- lib/subscriptionPlugin/subscriptions.d.ts
+++
@@ -1,3 +0,0 @@
-import { GeniePlugin } from '../GraphQLGenieInterfaces';
-declare const _default: () => GeniePlugin;
-export default _default;
